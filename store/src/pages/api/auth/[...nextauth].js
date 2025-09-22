2c7d55c636f90043f13260f334dd187665a2ec9b
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-09-22 12:51:20 +0530

    Update [...nextauth].js

--- store/src/pages/api/auth/[...nextauth].js
+++ store/src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
 // Create file: pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+//import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    //GoogleProvider({
+      //clientId: process.env.GOOGLE_CLIENT_ID,
+      //clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    //}),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
