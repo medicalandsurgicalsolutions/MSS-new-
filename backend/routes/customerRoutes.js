a1b0fd337d9fea379b666f7b92c09fa470e8050e
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-07 17:47:48 +0530

    Update customerRoutes.js

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -30,7 +30,7 @@ const {
 router.post("/verify-email", emailVerificationLimit, verifyEmailAddress);
 
 // shipping address send to array
-router.post("/shipping/address", addShippingAddress);
+router.post("/shipping/address/:id", addShippingAddress);
 
 // get all shipping address
 router.get("/shipping/address/:id", getShippingAddress);
