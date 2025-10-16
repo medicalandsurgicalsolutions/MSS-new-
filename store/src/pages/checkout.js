34f25e223489d1e773eff6c0c71fec452ca6f05f
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-16 06:54:39 +0000

    Update checkout.js

--- store/src/pages/checkout.js
+++ store/src/pages/checkout.js
@@ -233,6 +233,245 @@ const Checkout = () => {
                 {/* Personal details & shipping inputs remain same */}
                 {/* ...your existing code for InputArea and shipping remains unchanged... */}
 
+                 <div className="form-group">
+                    <h2 className="font-semibold  text-base text-gray-700 pb-3">
+                      01.{" "}
+                      {showingTranslateValue(
+                        storeCustomizationSetting?.checkout?.personal_details
+                      )}
+                    </h2>
+
+                    <div className="grid grid-cols-6 gap-6">
+                      <div className="col-span-6 sm:col-span-3">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.first_name
+                          )}
+                          name="firstName"
+                          type="text"
+                          placeholder="Enter your first name"
+                        />
+                        <Error errorName={errors.firstName} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.last_name
+                          )}
+                          name="lastName"
+                          type="text"
+                          placeholder="Enter your last name"
+                          required={false}
+                        />
+                        <Error errorName={errors.lastName} />
+                      </div>
+
+                      {/* If Stripe is enabled, uncomment CardElement import & block below */}
+                      {/* {showCard && (
+                        <div className="col-span-6">
+                          <CardElement />
+                          <p className="text-red-400 text-sm mt-1">{error}</p>
+                        </div>
+                      )} */}
+
+                      <div className="col-span-6 sm:col-span-3">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.checkout_phone
+                          )}
+                          name="phone"
+                          type="tel"
+                          placeholder="Enter phone number"
+                        />
+                        <Error errorName={errors.contact} />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="form-group mt-12">
+                    <h2 className="font-semibold  text-base text-gray-700 pb-3">
+                      02.{" "}
+                      {showingTranslateValue(
+                        storeCustomizationSetting?.checkout?.shipping_details
+                      )}
+                    </h2>
+
+                    <div className="grid grid-cols-6 gap-6 mb-8">
+                      <div className="col-span-6">
+                        <InputArea
+                          register={register}
+                          label={"Plat, House number, floor, building"}
+                          name="flat"
+                          type="text"
+                          placeholder="Eg: 402, Ground Floor"
+                        />
+                        <Error errorName={errors.flat} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.street_address
+                          )}
+                          name="address"
+                          type="text"
+                          placeholder="Eg: Patparganj Industrial Area, Sector 36"
+                        />
+                        <Error errorName={errors.address} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={"Landmark"}
+                          name="landmark"
+                          type="text"
+                          placeholder="Eg: Near Bagga Link"
+                        />
+                        <Error errorName={errors.landmark} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.city
+                          )}
+                          name="city"
+                          type="text"
+                          placeholder="Eg: New Delhi"
+                        />
+                        <Error errorName={errors.city} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={"District"}
+                          name="district"
+                          type="text"
+                          placeholder="Eg: New Delhi"
+                        />
+                        <Error errorName={errors.district} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={"State"}
+                          name="state"
+                          type="text"
+                          placeholder="Eg: Uttar Pradesh"
+                        />
+                        <Error errorName={errors.state} />
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.country
+                          )}
+                          name="country"
+                          type="text"
+                          placeholder="Eg: India"
+                        />
+                        <Error errorName={errors.country} />
+                      </div>
+
+                      <div
+                        id="PinInput"
+                        className="col-span-6 sm:col-span-3 lg:col-span-2"
+                      >
+                        <InputArea
+                          register={register}
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.zip_code
+                          )}
+                          value={inputPincode}
+                          name="zipCode"
+                          type="text"
+                          placeholder="Eg: 110092"
+                        />
+                        <Error errorName={errors.zipCode} />
+                      </div>
+                    </div>
+
+                    {storeCustomizationSetting?.checkout?.shipping_two_cost && (
+                      <>
+                        <Label
+                          label={showingTranslateValue(
+                            storeCustomizationSetting?.checkout?.shipping_cost
+                          )}
+                        />
+                        <div className="grid grid-cols-6 gap-6">
+                          {showingTranslateValue(
+                            storeCustomizationSetting?.checkout
+                              ?.shipping_one_desc
+                          ) && (
+                            <div className="col-span-6 sm:col-span-3">
+                              <InputShipping
+                                currency={currency}
+                                handleShippingCost={handleShippingCost}
+                                register={register}
+                                value={showingTranslateValue(
+                                  storeCustomizationSetting?.checkout
+                                    ?.shipping_name_two
+                                )}
+                                description={showingTranslateValue(
+                                  storeCustomizationSetting?.checkout
+                                    ?.shipping_one_desc
+                                )}
+                                cost={
+                                  Number(
+                                    storeCustomizationSetting?.checkout
+                                      ?.shipping_one_cost
+                                  ) || 60
+                                }
+                              />
+                              <Error errorName={errors.shippingOption} />
+                            </div>
+                          )}
+
+                          {showingTranslateValue(
+                            storeCustomizationSetting?.checkout
+                              ?.shipping_two_desc
+                          ) && (
+                            <div className="col-span-6 sm:col-span-3">
+                              <InputShipping
+                                currency={currency}
+                                handleShippingCost={handleShippingCost}
+                                register={register}
+                                value={showingTranslateValue(
+                                  storeCustomizationSetting?.checkout
+                                    ?.shipping_name_two
+                                )}
+                                description={showingTranslateValue(
+                                  storeCustomizationSetting?.checkout
+                                    ?.shipping_two_desc
+                                )}
+                                cost={
+                                  Number(
+                                    storeCustomizationSetting?.checkout
+                                      ?.shipping_two_cost
+                                  ) || 0
+                                }
+                              />
+                              <Error errorName={errors.shippingOption} />
+                            </div>
+                          )}
+                        </div>
+                      </>
+                    )}
+                  </div>
+
+                      // yha tak paste kiya hai
+
                 <div className="form-group mt-12">
                   <h2 className="font-semibold text-base text-gray-700 pb-3">
                     03. {showingTranslateValue(storeCustomizationSetting?.checkout?.payment_method)}
@@ -314,6 +553,140 @@ const Checkout = () => {
 
           {/* Order Summary - remains same */}
           {/* ...copy your existing CartItem & summary code here... */}
+
+          <div className="md:w-full lg:w-2/5 lg:ml-10 xl:ml-14 md:ml-6 flex flex-col h-full md:sticky lg:sticky top-28 md:order-2 lg:order-2">
+              <div className="border p-5 lg:px-8 lg:py-8 rounded-lg bg-white order-1 sm:order-2">
+                <h2 className="font-semibold  text-lg pb-4">
+                  {showingTranslateValue(
+                    storeCustomizationSetting?.checkout?.order_summary
+                  )}
+                </h2>
+
+                <div className="overflow-y-scroll flex-grow scrollbar-hide w-full max-h-64 bg-gray-50 block">
+                  {items.map((item) => (
+                    <CartItem key={item.id} item={item} currency={currency} />
+                  ))}
+
+                  {isEmpty && (
+                    <div className="text-center py-10">
+                      <span className="flex justify-center my-auto text-gray-500 font-semibold text-4xl">
+                        <IoBagHandle />
+                      </span>
+                      <h2 className="font-medium  text-sm pt-2 text-gray-600">
+                        No Item Added Yet!
+                      </h2>
+                    </div>
+                  )}
+                </div>
+
+                <div className="flex items-center mt-4 py-4 lg:py-4 text-sm w-full font-semibold text-heading last:border-b-0 last:text-base last:pb-0">
+                  <form className="w-full">
+                    {couponInfo.couponCode ? (
+                      <span className="bg-green-50 px-4 py-3 leading-tight w-full rounded-md flex justify-between">
+                        <p className="text-green-600">Coupon Applied </p>
+                        <span className="text-green-500 text-right">
+                          {couponInfo.couponCode}
+                        </span>
+                      </span>
+                    ) : (
+                      <div className="flex flex-col sm:flex-row items-start justify-end">
+                        <input
+                          ref={couponRef}
+                          type="text"
+                          placeholder={t("common:couponCode")}
+                          className="form-input py-2 px-3 md:px-4 w-full appearance-none transition ease-in-out border text-input text-sm rounded-md h-12 duration-200 bg-white border-gray-200 focus:ring-0 focus:outline-none focus:border-emerald-500 placeholder-gray-500 placeholder-opacity-75"
+                        />
+                        {isCouponAvailable ? (
+                          <button
+                            disabled={isCouponAvailable}
+                            type="submit"
+                            className="md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold text-center justify-center border border-gray-200 rounded-md placeholder-white focus-visible:outline-none focus:outline-none px-5 md:px-6 lg:px-8 py-3 md:py-3.5 lg:py-3 mt-3 sm:mt-0 sm:ml-3 md:mt-0 md:ml-3 lg:mt-0 lg:ml-3 hover:text-white hover:bg-emerald-500 h-12 text-sm lg:text-base w-full sm:w-auto"
+                          >
+                            <img
+                              src="/loader/spinner.gif"
+                              alt="Loading"
+                              width={20}
+                              height={10}
+                            />
+                            <span className=" ml-2 font-light">Processing</span>
+                          </button>
+                        ) : (
+                          <button
+                            disabled={isCouponAvailable}
+                            onClick={handleCouponCode}
+                            className="md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold text-center justify-center border border-gray-200 rounded-md placeholder-white focus-visible:outline-none focus:outline-none px-5 md:px-6 lg:px-8 py-3 md:py-3.5 lg:py-3 mt-3 sm:mt-0 sm:ml-3 md:mt-0 md:ml-3 lg:mt-0 lg:ml-3 hover:text-white hover:bg-emerald-500 h-12 text-sm lg:text-base w-full sm:w-auto"
+                          >
+                            {showingTranslateValue(
+                              storeCustomizationSetting?.checkout?.apply_button
+                            )}
+                          </button>
+                        )}
+                      </div>
+                    )}
+                  </form>
+                </div>
+
+                <div className="flex items-center py-2 text-sm w-full font-semibold text-gray-500 last:border-b-0 last:text-base last:pb-0">
+                  {showingTranslateValue(
+                    storeCustomizationSetting?.checkout?.sub_total
+                  )}
+                  <span className="ml-auto flex-shrink-0 text-gray-800 font-bold">
+                    {currency}
+                    {cartTotal?.toFixed(2)}
+                  </span>
+                </div>
+
+                <div className="flex items-center py-2 text-sm w-full font-semibold text-gray-500 last:border-b-0 last:text-base last:pb-0">
+                  {showingTranslateValue(
+                    storeCustomizationSetting?.checkout?.shipping_cost
+                  )}
+                  {deliveryChargeToApply > 0 ? (
+                    <span className="ml-auto flex-shrink-0 text-gray-800 font-bold">
+                      {currency}
+                      {deliveryChargeToApply?.toFixed(2)}
+                    </span>
+                  ) : (
+                    <span className="ml-auto flex-shrink-0 text-green-600 font-bold">
+                      Free Delivery
+                    </span>
+                  )}
+                </div>
+
+                {codDisplay > 0 && (
+                  <div className="flex items-center py-2 text-sm w-full font-semibold text-red-600 last:border-b-0 last:text-base last:pb-0">
+                    COD Charge
+                    <span className="ml-auto flex-shrink-0 font-bold text-red-600">
+                      {currency}
+                      {codDisplay.toFixed(2)}
+                    </span>
+                  </div>
+                )}
+
+                <div className="flex items-center py-2 text-sm w-full font-semibold text-green-600 last:border-b-0 last:text-base last:pb-0">
+                  {showingTranslateValue(
+                    storeCustomizationSetting?.checkout?.discount
+                  )}
+                  <span className="ml-auto flex-shrink-0 font-bold text-green-600">
+                    {currency}
+                    {discountAmount.toFixed(2)}
+                  </span>
+                </div>
+
+                <div className="border-t mt-4">
+                  <div className="flex items-center font-bold justify-between pt-5 text-sm uppercase">
+                    {showingTranslateValue(
+                      storeCustomizationSetting?.checkout?.total_cost
+                    )}
+                    <span className="font-extrabold text-lg">
+                      {currency}
+                      {parseFloat(total).toFixed(2)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+                      // yha tk paste kiya hai
         </div>
       </div>
     </Layout>
