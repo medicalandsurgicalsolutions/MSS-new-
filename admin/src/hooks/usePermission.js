e0c704d41cbe3d1bce3ceb46bb05cd5c7178605d
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-17 13:59:04 +0530

    Update usePermission.js

--- admin/src/hooks/usePermission.js
+++ admin/src/hooks/usePermission.js
@@ -1,12 +1,12 @@
-import hasPermission from "@/utils/hasPermission"; // keep same import
+import useHasPermission from "@/utils/hasPermission"; // same import
 import { notifyError } from "@/utils/toast";
 
 const usePermission = (name, editFunction = null, deleteFunction = null) => {
-  const canAdd = hasPermission(name, "Add");
-  const canEdit = hasPermission(name, "Edit");
-  const canList = hasPermission(name, "List");
-  const canShow = hasPermission(name, "Show");
-  const canDelete = hasPermission(name, "Delete");
+  const canAdd = useHasPermission(name, "Add");
+  const canEdit = useHasPermission(name, "Edit");
+  const canList = useHasPermission(name, "List");
+  const canShow = useHasPermission(name, "Show");
+  const canDelete = useHasPermission(name, "Delete");
 
   const can = { add: canAdd, edit: canEdit, list: canList, show: canShow, delete: canDelete };
 
