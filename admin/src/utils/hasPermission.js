bd1c1c483e3af9e6bdd4c0108d73e9cf0bba3216
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-17 13:57:48 +0530

    Update hasPermission.js

--- admin/src/utils/hasPermission.js
+++ admin/src/utils/hasPermission.js
@@ -1,19 +1,18 @@
+// hasPermission.js
 import { useSelector } from "react-redux";
 
-const hasPermission = (permission, name) => {
+const useHasPermission = (permission, name) => {
   const settings = useSelector((state) => state.setting.settingItem);
 
   const permissions = settings?.find(
     (value) => value.name === "permissionSetting"
   )?.permissions;
 
-  if (!permissions) {
-    return false;
-  }
+  if (!permissions) return false;
 
   return permissions.some(
     (item) => item.permission === permission && item.name.en === name
   );
 };
 
-export default hasPermission;
+export default useHasPermission;
