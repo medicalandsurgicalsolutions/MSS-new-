93b2b5c335fa5634c60e0bbc970d6769bb1fecaa
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-17 12:12:47 +0530

    Update description privacy-policy.js

--- store/src/pages/privacy-policy.js
+++ store/src/pages/privacy-policy.js
@@ -13,7 +13,7 @@ const PrivacyPolicy = () => {
   // console.log("data", storeCustomizationSetting);
 
   return (
-    <Layout title="Privacy Policy" description="This is privacy policy page">
+    <Layout title="Privacy Policy" description="Read the Privacy Policy of Medical & Surgical Solutions to understand how we collect, use, and protect your personal and payment information securely.">
       <PageHeader
         headerBg={storeCustomizationSetting?.privacy_policy?.header_bg}
         title={showingTranslateValue(
