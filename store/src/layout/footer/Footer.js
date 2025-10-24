0886297e1f656c5d9962f78095d05cd78c6cec84
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-24 15:29:49 +0530

    Update Footer.js

--- store/src/layout/footer/Footer.js
+++ store/src/layout/footer/Footer.js
@@ -174,19 +174,19 @@ const Footer = () => {
         </h4>
          <div className="flex gap-2 justify-center md:justify-start">
           <Link href="https://www.facebook.com/share/1BKFiDnANi/" target="_blank" rel="noopener noreferrer"className="bg-white p-1 rounded-full shadow hover:bg-blue-100 transition">
-            <FacebookIcon size={22} round />
+            <FacebookIcon size={26} round />
           </Link>
           <Link href="https://www.instagram.com/mssofficial2011/" target="_blank" rel="noopener noreferrer"className="bg-white p-1 rounded-full shadow hover:bg-pink-100 transition">
-            <FaInstagram size={20} className="text-pink-500 hover:text-pink-600 transition" />
+            <FaInstagram size={24} className="text-pink-500 hover:text-pink-600 transition" />
           </Link>
           <Link href="https://www.linkedin.com/company/105331627/admin/page-posts/published/" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full shadow hover:bg-blue-100 transition">
-            <LinkedinIcon size={22} round />
+            <LinkedinIcon size={26} round />
           </Link>
           <Link href="https://wa.me/9643344588" target="_blank" rel="noopener noreferrer"className="bg-white p-1 rounded-full shadow hover:bg-green-100 transition">
-            <WhatsappIcon size={22} round />
+            <WhatsappIcon size={26} round />
           </Link>
           <Link href="https://www.youtube.com/@MEDICALANDSURGICALSOLUTIONS" target="_blank" rel="noopener noreferrer" className="bg-white p-1 rounded-full shadow hover:bg-red-100 transition">
-            <FaYoutube size={20} className="text-red-500 hover:text-red-600 transition" />
+            <FaYoutube size={24} className="text-red-500 hover:text-red-600 transition" />
           </Link>
         </div>
       </div>
