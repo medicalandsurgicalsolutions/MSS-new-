15c82c4c566a31619de89dae55340b98580c4267
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-10-28 17:32:57 +0530

    right aligned all nav top item NavBarTop.js

--- store/src/layout/navbar/NavBarTop.js
+++ store/src/layout/navbar/NavBarTop.js
@@ -56,7 +56,7 @@ const NavBarTop = () => {
     <>
       <div className="hidden lg:block bg-gray-100">
         <div className="max-w-screen-2xl mx-auto px-3 sm:px-10">
-          <div className="text-gray-700 py-2 font-sans text-xs font-medium border-b flex justify-between items-center">
+          <div className="text-gray-700 py-2 text-xs font-medium border-b flex justify-end items-center">
             {/* 
                <span className="flex items-center font-bold">
                   <FiPhoneCall className="mr-2" />
