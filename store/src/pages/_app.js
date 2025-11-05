ad56c0329e110e03d00bb4cd61ad9cc05158522b
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-11-05 17:41:39 +0530

    Update _app.js

--- store/src/pages/_app.js
+++ store/src/pages/_app.js
@@ -66,7 +66,7 @@ function MyApp({ Component, pageProps }) {
             <style>{`
           /* Apply Doppio One to all headings */
           h1, h2, h3, h4, h5, h6 {
-            font-family: 'Windsor', 'serif' !important;
+            font-family: "Allura", "Delius", "cursive" !important;
           }
         `}</style>
       </Head>
