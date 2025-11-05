626cfdb420a2bb57108bd0ecfa36794f9b1639dd
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-11-05 17:45:03 +0530

    Update _app.js

--- store/src/pages/_app.js
+++ store/src/pages/_app.js
@@ -59,14 +59,15 @@ function MyApp({ Component, pageProps }) {
     <>
      {/* ✅ Google Fonts Import (Montserrat) */}
       <Head>
-        <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Doppio+One&display=swap"
+        rel="stylesheet"
+      />
+
             <style>{`
           /* Apply Doppio One to all headings */
           h1, h2, h3, h4, h5, h6 {
-            font-family: "Allura", "Delius", "cursive" !important;
+            font-family: Doppio One", "sans-serif,!important;
           }
         `}</style>
       </Head>
