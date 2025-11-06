320d8b39e6cfd075d331f61683e4d0c1d0fd1e0a
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-11-06 11:43:31 +0530

    Update the Layout.js

--- store/src/layout/Layout.js
+++ store/src/layout/Layout.js
@@ -60,11 +60,15 @@ const Layout = ({ title, description, children }) => {
       </div>
 
       {/* Floating WhatsApp button */}
-     <div
-      className="
-        fixed bottom-24 sm:bottom-28 md:bottom-28 lg:bottom-20 right-3 
-        bg-green-500 text-white rounded-full p-3 cursor-pointer shadow-lg hover:bg-green-600 transition duration-300"
-        style={{ zIndex: 1000 }}>
+    <div
+        className="
+          fixed 
+          bottom-24 lg:bottom-20 right-3 
+          bg-green-500 text-white rounded-full p-3 cursor-pointer shadow-lg 
+          hover:bg-green-600 transition duration-300
+        "
+        style={{ zIndex: 1000 }}
+      >
         <a
           href={`https://wa.me/${storeCustomizationSetting?.footer?.block4_phone?.replace(/\D/g, '')}`}
           target="_blank"
@@ -72,17 +76,25 @@ const Layout = ({ title, description, children }) => {
         >
           <FaWhatsapp size={20} />
         </a>
-      </div>
+    </div>
+
 
       {/* Scroll to top button (below WhatsApp) */}
       {showScroll && (
      <div
-        onClick={scrollToTop}
-        className="fixed bottom-[3.5rem] right-3 bg-[#0891b2] text-white rounded-full p-3 cursor-pointer shadow-lg hover:bg-[#b52228] transition duration-300"
-        style={{ zIndex: 1000 }}
-      >
-        <FaArrowUp size={18} />
-      </div>
+      onClick={scrollToTop}
+      className="
+        fixed 
+        bottom-4 sm:bottom-[3.5rem] md:bottom-[3.5rem] 
+        right-2 
+        bg-[#0891b2] text-white rounded-full p-3 cursor-pointer shadow-lg 
+        hover:bg-[#b52228] transition duration-300
+      "
+      style={{ zIndex: 1000 }}
+    >
+      <FaArrowUp size={20} />
+    </div>
+
 
       )}
     </>
