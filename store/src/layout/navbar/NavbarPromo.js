a3b99ab92f8717e07043ffdec94a077b6cafa07a
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-11-06 13:10:11 +0530

    Update NavbarPromo.js

--- store/src/layout/navbar/NavbarPromo.js
+++ store/src/layout/navbar/NavbarPromo.js
@@ -94,114 +94,121 @@ const NavbarPromo = () => {
   return (
     <>
       <div className="hidden lg:block xl:block bg-gray-100 border-b text-sm text-black">
-        <div className="max-w-screen-2xl mx-auto px-6 sm:px-8 lg:px-10 flex justify-between items-center">
-          <div>
-            <Link
-              onClick={() => setIsLoading(!isLoading)}
-              href="/"
-              className="mx-4 py-2 hover:text-emerald-600"
-            >
-              Home
-            </Link>
-          </div>
+  <div
+    className="
+      max-w-screen-2xl
+      mx-auto
+      px-6 sm:px-8 lg:px-10
+      flex flex-nowrap items-center justify-between
+      overflow-x-auto
+      scrollbar-hide
+      whitespace-nowrap
+    "
+  >
+    <div>
+      <Link
+        onClick={() => setIsLoading(!isLoading)}
+        href="/"
+        className="mx-4 py-2 hover:text-emerald-600"
+      >
+        Home
+      </Link>
+    </div>
 
-          <div>
-            {storeCustomizationSetting?.home?.quick_delivery_subtitle?.en && (
-              <>
-                <Link
-                  onClick={() => setIsLoading(!isLoading)}
-                  href="/search?query=latest"
-                >
-                  <div className="mx-4 py-2 hover:text-emerald-600">
-                    {
-                      storeCustomizationSetting?.home?.quick_delivery_subtitle
-                        ?.en
-                    }
-                  </div>
-                </Link>
-              </>
-            )}
-          </div>
-
-          {data[0]?.children?.slice(0, 6)?.map((category, index) => (
-            <div
-              key={index}
-              className="relative cursor-pointer group py-2"
-              onClick={() =>
-                handleSubCategory(
-                  category?._id,
-                  showingTranslateValue(category?.name)
-                )
+    <div>
+      {storeCustomizationSetting?.home?.quick_delivery_subtitle?.en && (
+        <>
+          <Link
+            onClick={() => setIsLoading(!isLoading)}
+            href="/search?query=latest"
+          >
+            <div className="mx-4 py-2 hover:text-emerald-600">
+              {
+                storeCustomizationSetting?.home?.quick_delivery_subtitle
+                  ?.en
               }
-            >
-              <div className="mx-4 group hover:text-emerald-600 flex items-center space-x-2">
-                <div>{capitalizeWords(category?.name?.en)}</div>
-                {category?.children && (
-                  <div className="group-hover:rotate-180 duration-200 py-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-3"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                      />
-                    </svg>
-                  </div>
-                )}
-              </div>
+            </div>
+          </Link>
+        </>
+      )}
+    </div>
 
-              {category?.children && (
-                <div className="absolute left-0 w-60 top-full hidden group-hover:block shadow-lg">
-                  {category?.children && (
+    {data[0]?.children?.slice(0, 6)?.map((category, index) => (
+      <div
+        key={index}
+        className="relative cursor-pointer group py-2"
+        onClick={() =>
+          handleSubCategory(
+            category?._id,
+            showingTranslateValue(category?.name)
+          )
+        }
+      >
+        <div className="mx-4 group hover:text-emerald-600 flex items-center space-x-2">
+          <div>{capitalizeWords(category?.name?.en)}</div>
+          {category?.children && (
+            <div className="group-hover:rotate-180 duration-200 py-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-3"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </div>
+          )}
+        </div>
+
+        {category?.children && (
+          <div className="absolute left-0 w-60 top-full hidden group-hover:block shadow-lg">
+            {category?.children && (
+              <div
+                className="absolute left-0 w-auto top-full rounded-md hidden group-hover:block bg-cyan-500 text-white shadow-lg p-4 gap-y-2 gap-x-6"
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: `repeat(${Math.ceil(
+                    category.children.length / 8
+                  )}, auto)`,
+                }}
+              >
+                {category?.children?.map((subCategory, subIndex) => (
+                  <div className="border-b" key={subIndex}>
                     <div
-                      className="absolute left-0 w-auto top-full rounded-md hidden group-hover:block bg-cyan-500 text-white shadow-lg p-4 gap-y-2 gap-x-6"
-                      style={{
-                        display: "grid",
-                        gridTemplateColumns: `repeat(${Math.ceil(
-                          category.children.length / 8
-                        )}, auto)`,
+                      className="block px-1 text-sm cursor-pointer py-1 hover:translate-x-1.5 duration-100 whitespace-nowrap"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        handleSubNestedCategory(
+                          subCategory?._id,
+                          showingTranslateValue(subCategory?.name)
+                        );
                       }}
                     >
-                      {category?.children?.map((subCategory, subIndex) => (
-                        <div className="border-b" key={subIndex}>
-                          <div
-                            className="block px-1 text-sm cursor-pointer py-1 hover:translate-x-1.5 duration-100 whitespace-nowrap"
-                            onClick={(event) => {
-                              event.stopPropagation();
-                              handleSubNestedCategory(
-                                subCategory?._id,
-                                showingTranslateValue(subCategory?.name)
-                              );
-                            }}
-                          >
-                            {subCategory?.name?.en}
-                          </div>
-                        </div>
-                      ))}
+                      {subCategory?.name?.en}
                     </div>
-                  )}
-                </div>
-              )}
-            </div>
-          ))}
-
-          {/* ✅ map closed properly before next JSX */}
-          <div>
-            <Link
-              onClick={() => setIsLoading(!isLoading)}
-              href="/contact-us"
-            >
-              <div className="mx-4 py-2 hover:text-emerald-600">Buy In Bulk</div>
-            </Link>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </div>
+    ))}
+
+    <div>
+      <Link onClick={() => setIsLoading(!isLoading)} href="/contact-us">
+        <div className="mx-4 py-2 hover:text-emerald-600">Buy In Bulk</div>
+      </Link>
+    </div>
+  </div>
+</div>
+
     </>
   );
 };
