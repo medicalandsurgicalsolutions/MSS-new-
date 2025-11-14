4ef0557d12b423467e3a91f8841f8bee26e6c5ed
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-11-14 15:39:21 +0530

    Update Layout.js

--- store/src/layout/Layout.js
+++ store/src/layout/Layout.js
@@ -34,6 +34,21 @@ const Layout = ({ title, description, children }) => {
 
   return (
     <>
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FQFCKJZ2DF"
+      />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FQFCKJZ2DF');
+          `,
+        }}
+      />
       <ToastContainer />
 
       <div className="font-serif">
