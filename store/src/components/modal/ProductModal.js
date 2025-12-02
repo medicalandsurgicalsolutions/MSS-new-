6f3d5397d9034e757a7a449b81967539f6d90db7
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-12-02 15:10:02 +0530

    Update the logo position ProductModal.js

--- store/src/components/modal/ProductModal.js
+++ store/src/components/modal/ProductModal.js
@@ -130,9 +130,20 @@ const ProductModal = ({ modalOpen, setModalOpen, product, attributes, currency }
                 {discount > 0 && (
                   <Discount product={product} discount={discount} modal />
                 )}
-              </div> */}
+              </div>*/}
 
-              <div className="absolute top-2 left-2 z-10 w-full"> {discount > 0 && ( <Discount product={product} discount={discount} modal /> )} </div>
+              
+              {/* Discount at bottom-right */}
+                    <div className="absolute bottom-2 right-2 z-10">
+                      {discount > 0 && (
+                        <Discount product={product} discount={discount} modal />
+                      )}
+                    </div>
+                  </div>
+
+                  {/* <div className="absolute top-2 left-2 z-10 w-full"> 
+                     {discount > 0 && ( <Discount product={product} discount={discount} modal /> )} 
+                  </div> */}
 
                 {/* Logo Right */}
                 <div className="absolute top-2 right-2 z-10">
