0ec48a680b3a4ae8cd38686f93d29417ec4a721d
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2025-12-18 12:58:24 +0530

    Update CustomSelect.jsx

--- admin/src/components/form/selectOption/CustomSelect.jsx
+++ admin/src/components/form/selectOption/CustomSelect.jsx
@@ -3,24 +3,30 @@ import { Select } from "@windmill/react-ui";
 import useUtilsFunction from "@/hooks/useUtilsFunction";
 
 const CustomSelect = ({ setData, register, name, label, objectList = [] }) => {
-
   const { showingTranslateValue } = useUtilsFunction();
 
   return (
-    <>
-      <Select
-        onChange={(e) => setData(e.target.value)}
-        name={name}
-        {...register(`${name}`, {
-          required: `${label} is required!`,
-        })}
-      >
-        <option value="" defaultValue hidden>{label}</option>
-        {objectList.map(item => (
-          <option key={item?._id} value={item?._id}>{showingTranslateValue(item?.name || item?.title) || item?.name}</option>
-        ))}
-      </Select>
-    </>
+    <Select
+      name={name}
+      {...register(name, {
+        required: `${label} is required!`,
+      })}
+      onChange={(e) => {
+        if (setData) {
+          setData(e.target.value);
+        }
+      }}
+    >
+      <option value="" hidden>
+        {label}
+      </option>
+
+      {objectList?.map((item) => (
+        <option key={item?._id} value={item?._id}>
+          {showingTranslateValue(item?.name || item?.title) || item?.name}
+        </option>
+      ))}
+    </Select>
   );
 };
 
