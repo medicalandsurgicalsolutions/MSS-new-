98eea0624fae3e8928397ca1d897a4dc87e47622
medicalandsurgicalsolutions <medicalandsurgicalsolutions@gmail.com>
2026-01-08 17:14:32 +0530

    Update the COD OrderInvoice.jsx

--- admin/src/pages/OrderInvoice.jsx
+++ admin/src/pages/OrderInvoice.jsx
@@ -188,17 +188,7 @@ const OrderInvoice = () => {
                   {data.paymentMethod}
                 </span>
               </div>
-              /* {data.paymentMethod === "Cash" && (
-                <div className="mb-3 md:mb-0 lg:mb-0  flex flex-col sm:flex-wrap">
-                  <span className="mb-1 font-bold font-serif text-sm uppercase text-black dark:text-gray-500 block">
-                    {"COD Charge"}
-                  </span>
-                  <span className="text-sm text-black dark:text-gray-400 font-semibold font-serif block">
-                    {currency}
-                    {"60"}
-                  </span>
-                </div>
-              )} */
+             
               <div className="mb-3 md:mb-0 lg:mb-0  flex flex-col sm:flex-wrap">
                 <span className="mb-1 font-bold font-serif text-sm uppercase text-black dark:text-gray-500 block">
                   {t("ShippingCost")}
